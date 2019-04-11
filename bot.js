2384be49dbde0748cc48dcdcb295bb4ce7750141
x5ox <37715638+unwell35@users.noreply.github.com>
2019-04-12 02:29:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,46 +1,38 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const moha = new Discord.Client();
 const adminprefix = "+";
 const developers = ['399250242136047619'];
 
-console.log("Mohaaaaaaaaa");
-
-client.on('ready', () => {
-    console.log(`Logged as ${client.user.tag}`)
-})
-
-client.on('message', message => {
+moha.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
   if (message.content.startsWith(adminprefix + 'ply')) {
-    client.user.setGame(argresult);
+    moha.user.setGame(argresult);
       message.channel.send(`تم تغيير البلاينق الى   ${argresult}`)
   } else 
      if (message.content === (adminprefix + "leave")) {
     message.guild.leave();        
   } else  
   if (message.content.startsWith(adminprefix + 'wt')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
+  moha.user.setActivity(argresult, {type:'WATCHING'});
       message.channel.send(`تَم تغيير الواتشينق الى   ${argresult}`)
   } else 
   if (message.content.startsWith(adminprefix + 'ls')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
+  moha.user.setActivity(argresult , {type:'LISTENING'});
       message.channel.send(`تَم تغيير الليسينينق الى   ${argresult}`)
   } else
   if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/moha");
+    moha.user.setGame(argresult, "https://www.twitch.tv/moha");
       message.channel.send(`تم تغييرك حالتك بالتويتش الى   ${argresult}`)
   }
   if (message.content.startsWith(adminprefix + 'sn')) {
-  client.user.setUsername(argresult).then
+  moha.user.setUsername(argresult).then
       message.channel.send(`جاري تغيير الأسم لـ ..${argresult} `)
 } else
 if (message.content.startsWith(adminprefix + 'sa')) {
-  client.user.setAvatar(argresult);
+  moha.user.setAvatar(argresult);
     message.channel.send(`جاري تغيير الأفتار... : `);
 }
 });
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-client.login(process.env.BOT_TOKEN);
+
+moha.login(process.env.BOT_TOKEN);
