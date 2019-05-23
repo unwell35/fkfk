9ab8777a8eddad04f619e85913a4796f3f82a738
x5ox <37715638+unwell35@users.noreply.github.com>
2019-05-23 23:09:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,38 +1,46 @@
-const Discord = require('discord.js');
-const moha = new Discord.Client();
-const adminprefix = "+";
-const developers = ['399250242136047619'];
-
-moha.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
-  if (message.content.startsWith(adminprefix + 'ply')) {
-    moha.user.setGame(argresult);
-      message.channel.send(`تم تغيير البلاينق الى   ${argresult}`)
-  } else 
-     if (message.content === (adminprefix + "leave")) {
-    message.guild.leave();        
-  } else  
-  if (message.content.startsWith(adminprefix + 'wt')) {
-  moha.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`تَم تغيير الواتشينق الى   ${argresult}`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'ls')) {
-  moha.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`تَم تغيير الليسينينق الى   ${argresult}`)
-  } else
-  if (message.content.startsWith(adminprefix + 'st')) {
-    moha.user.setGame(argresult, "https://www.twitch.tv/moha");
-      message.channel.send(`تم تغييرك حالتك بالتويتش الى   ${argresult}`)
-  }
-  if (message.content.startsWith(adminprefix + 'sn')) {
-  moha.user.setUsername(argresult).then
-      message.channel.send(`جاري تغيير الأسم لـ ..${argresult} `)
-} else
-if (message.content.startsWith(adminprefix + 'sa')) {
-  moha.user.setAvatar(argresult);
-    message.channel.send(`جاري تغيير الأفتار... : `);
-}
+const Discord = require('discord.js'); 
+const client = new Discord.Client();
+const fs = require('fs');
+const status = JSON.parse(fs.readFileSync('./status.json' , 'utf8'));
+const prefix = "+";
+client.on("ready",() => { //iTzMurtaja
+console.log("Ready!"); //iTzMurtaja
+if(!status[client.user.id]) {status[client.user.id] = {
+    text: client.user.tag,
+    type: "WATCHING"
+}} //iTzMurtaja
+client.user.setActivity(status[client.user.id].text, {type: status[client.user.id].type})    //iTzMurtaja
 });
-
-moha.login(process.env.BOT_TOKEN);
+ 
+client.on("message", message => { //iTzMurtaja
+if(message.content.startsWith(prefix + "pre")) { //iTzMurtaja
+if(message.author.id !== "479090634813341696") return; //iTzMurtaja
+var args = message.content.split(" ").slice(1).join(" "); //iTzMurtaja
+status[client.user.id].text = args //iTzMurtaja
+message.reply("Done, I set it to \`" + args +"\`") //iTzMurtaja
+  client.user.setActivity(status[client.user.id].text,{  type : status[client.user.id].type})} //iTzMurtaja
+fs.writeFile("./status.json", JSON.stringify(status), (err) => { //iTzMurtaja
+if (err) console.error(err) //iTzMurtaja
+}) //iTzMurtaja
+}); //iTzMurtaja
+ 
+client.on("message", message => { //iTzMurtaja
+if (message.author.id !== "479090634813341696") return; //iTzMurtaja
+  if(message.content === prefix + "watching") { //iTzMurtaja
+status[client.user.id].type = "WATCHING" //iTzMurtaja
+message.reply("Done, I set it to \`WATCHING\`") //iTzMurtaja
+client.user.setActivity(status[client.user.id], {type: status[client.user.id].type})} //iTzMurtaja
+else //iTzMurtaja
+if(message.content === prefix + "listening") { //iTzMurtaja
+status[client.user.id].type = "LISTENING" //iTzMurtaja
+message.reply("Done, I set it to \`LISTENING\`") //iTzMurtaja
+client.user.setActivity(status[client.user.id], {type: status[client.user.id].type})} //iTzMurtaja
+else //iTzMurtaja
+if(message.content === prefix + "playing") { //iTzMurtaja
+status[client.user.id].type = "PLAYING" //iTzMurtaja
+message.reply("Done, I set it to \`PLAYING\`") //iTzMurtaja
+client.user.setActivity(status[client.user.id], {type: status[client.user.id].type})} //iTzMurtaja
+fs.writeFile("./status.json", JSON.stringify(status), (err) => { //iTzMurtaja
+if (err) console.error(err) //iTzMurtaja
+})})
+client.login(process.env.BOT_TOKEN);
